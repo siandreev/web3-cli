339d7857ca00aa507a253645a3e947f367a29ff1
siandrev <andreev.sergey.i@yandex.ru>
2021-09-07 13:57:23 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+## Web3-cli
+
+### Get started
+1. Create `.env` file following format
+```
+KOVAN_RPC=<rpc_link>
+GOERLI_RPC=<rpc_link>
+ROPSTEN_RPC=<rpc_link>
+MAINNET_RPC=<rpc_link>
+BINANCE_RPC=<rpc_link>
+MATIC_RPC=https://rpc-mainnet.maticvigil.com/
+XDAI_RPC=https://rpc.xdaichain.com/
+GANACHE=<ganache_sandbox_url>
+LOCAL=<local_rpc_url>
+PRIVATE_KEY=<private_key>
+LOCAL_KEY=<private_key_for_local_node>
+GANACHE_KEY=<private_key_for_ganache>
+```
+
+2. Run `yarn` to install dependencies.
+
+3. Add following to `.zshrc` or `.bashrc`
+`alias web3="node --experimental-repl-await <PATH_TO_PROJECT>/index.js"`
+
+### Usage
+1. Type `web3` in terminal to run web3-cli
+
+2. Change the selected blockchain by calling the corresponding function, e.g. `mainnet()` to select ethereum mainnet. The default chain is kovan.
+
+3. Call any web3 method using repl await and injected web3:
+```javascript
+web3> await web3.eth.getBalance(address)
+```
+
---
+++ index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+import repl from 'repl';
+import Web3 from 'web3';
+import dotenv from 'dotenv';
+import path from "path";
+
+dotenv.config({ path:path.join(process.argv[1], '..', '.env')});
+const web3 = new Web3(process.env.KOVAN_RPC);
+web3.eth.accounts.wallet.add(process.env.PRIVATE_KEY);
+
+const cli = repl.start('web3> ');
+
+cli.context.web3 = web3;
+cli.context.address = web3.eth.accounts.wallet[0].address;
+cli.context.log = console.log;
+cli.context.goerli = () => setNewWeb3('goerli', process.env.GOERLI_RPC);
+cli.context.kovan = () => setNewWeb3('kovan', process.env.KOVAN_RPC);
+cli.context.ropsten = () => setNewWeb3('ropsten', process.env.ROPSTEN_RPC);
+cli.context.local = () => setNewWeb3('local', process.env.LOCAL, process.env.LOCAL_KEY);
+cli.context.mainnet = () => setNewWeb3('ethereum mainnet', process.env.MAINNET_RPC);
+cli.context.matic = () => setNewWeb3('matic', process.env.MATIC_RPC);
+cli.context.ganache = () => setNewWeb3('ganache', process.env.GANACHE, process.env.GANACHE_KEY);
+cli.context.bsc = () => setNewWeb3('bsc', process.env.BINANCE_RPC);
+cli.context.xDai = () => setNewWeb3('xDai', process.env.XDAI_RPC);
+
+function setNewWeb3(title, provider, key = process.env.PRIVATE_KEY) {
+    const web3 = new Web3(provider);
+    web3.eth.accounts.wallet.add(key);
+    cli.context.web3 = web3;
+    cli.context.address = web3.eth.accounts.wallet[0].address;
+    return `Now using ${title} blockchain`
+}
+
